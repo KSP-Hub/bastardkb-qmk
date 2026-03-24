ecf24ff8a933d1b19655556c492c8bb731455f6a
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-03-24 14:17:32 +0300

    Update keymap.c

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
@@ -1,11 +1,3 @@
-#include QMK_KEYBOARD_H
-
-// Настройка RGB для двух половин
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_SPLIT
-#    define RGBLED_SPLIT { 35, 35 } 
-#endif
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_charybdis_4x6(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
