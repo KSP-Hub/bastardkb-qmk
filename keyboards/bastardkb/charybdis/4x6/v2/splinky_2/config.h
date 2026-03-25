5776e74b449fdd4819b7c8d83925437fc0123a75
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-03-25 14:23:01 +0300

    Final fix: correct RGB configuration for Charybdis 4x6

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,29 +1,12 @@
-/*
- * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 /* RGB Matrix */
 #undef RGB_MATRIX_ENABLE
 #define RGB_MATRIX_ENABLE yes
 
 #undef RGB_MATRIX_LED_COUNT
-#define RGB_MATRIX_LED_COUNT 58
+#define RGB_MATRIX_LED_COUNT 58  // Стандартное значение для Charybdis 4x6
 
 #undef RGB_MATRIX_SPLIT
-#define RGB_MATRIX_SPLIT { 29, 29 }
+#define RGB_MATRIX_SPLIT { 29, 29 }  // Стандартное разделение для QMK
 
 #undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
@@ -36,11 +19,6 @@
 
 /* Handedness. */
 #define MASTER_RIGHT
-
-// To use the handedness pin, resistors need to be installed on the adapter PCB.
-// If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
-// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 /* VBUS detection. */
 #define USB_VBUS_PIN GP25
