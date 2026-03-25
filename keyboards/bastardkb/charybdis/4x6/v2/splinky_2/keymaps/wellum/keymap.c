423efab4a8c9521d7d92818ac1bb489efa53248b
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-03-25 19:00:38 +0300

    fix(keymap): добавление недостающей клавиши для LAYOUT_charybdis_4x6
    Решение критической ошибки компиляции:
    
    1. Исправление количества клавиш:
       - Добавлена недостающая клавиша KC_TRNS в пятую строку каждого слоя
       - Теперь в каждом слое 56 клавиш (12+12+12+12+8), что соответствует ожидаемому макросом LAYOUT
       - Устранена ошибка "macro 'LAYOUT' requires 56 arguments, but only 55 given"
    
    2. Исправление заголовочного файла:
       - Заменено #include "qmk_keyboard.h" на #include QMK_KEYBOARD_H
       - Это правильный синтаксис для QMK
       - Устраняет потенциальную ошибку "fatal error: qmk_keyboard.h: No such file or directory"
    
    Это финальное исправление, которое позволит успешно завершить компиляцию проекта.

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LGUI, KC_LALT, KC_SPC,  MO(1),   MO(2),   KC_ENT,  KC_BTN1
+        KC_LGUI, KC_LALT, KC_SPC,  MO(1),   MO(2),   KC_ENT,  KC_BTN1, KC_TRNS
     ),
     
     [1] = LAYOUT_charybdis_4x6(
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, MO(0),   _______, KC_TRNS, KC_TRNS
+        _______, _______, _______, MO(0),   _______, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     
     [2] = LAYOUT_charybdis_4x6(
@@ -29,6 +29,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, MO(0),   _______, KC_TRNS, KC_TRNS
+        _______, _______, _______, MO(0),   _______, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
