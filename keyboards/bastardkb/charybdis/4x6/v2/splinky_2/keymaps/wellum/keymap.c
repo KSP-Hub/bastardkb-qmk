caf51b62044ebe983cbeeddb4b14063090097db6
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-03-27 20:02:00 +0300

    feat(via): добавление поддержки VIA в keymap.c с сохранением текущей раскладки

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
@@ -1,3 +1,10 @@
+#ifdef VIA_ENABLE
+#    include "via.h"
+#    ifndef LAYER_STATE_8BIT
+#        define LAYER_STATE_8BIT
+#    endif
+#endif
+
 #include QMK_KEYBOARD_H
 
 #ifdef RGB_MATRIX_ENABLE
@@ -8,7 +15,11 @@ void keyboard_post_init_user(void) {
 #endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+#ifdef VIA_ENABLE
+    [_VIA1] = LAYOUT_charybdis_4x6(
+#else
     [0] = LAYOUT_charybdis_4x6(
+#endif
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
@@ -31,4 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, MO(0),   _______, KC_TRNS, KC_TRNS, KC_TRNS
     )
+#ifdef VIA_ENABLE
 };
+const uint16_t *const PROGMEM keymaps[] = {
+    [0] = keymaps[_VIA1],
+    [_VIA1] = keymaps[_VIA1],
+    [_VIA2] = keymaps[1],
+    [_VIA3] = keymaps[2]
+};
+#endif
