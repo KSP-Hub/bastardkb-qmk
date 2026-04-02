094cc9780b177c68d28219dd1808cfb68df7d6af
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-04-02 17:07:32 +0300

    fix: final keymap layout for 29/27 keys and RP2040 boot

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
@@ -1,19 +1,12 @@
+#include QMK_KEYBOARD_H
+
 #ifdef VIA_ENABLE
 #    include "via.h"
-#    ifndef LAYER_STATE_16BIT
-#        define LAYER_STATE_16BIT
-#    endif
 #endif
-
-#include QMK_KEYBOARD_H
 
 #ifdef RGB_MATRIX_ENABLE
 void keyboard_post_init_user(void) {
-    #ifdef MASTER_RIGHT
-    if (is_keyboard_right()) {
-    #else
-    if (is_keyboard_left()) {
-    #endif
+    if (is_keyboard_master()) {
         rgb_matrix_mode(RGB_MATRIX_SOLID_COLOR);
         rgb_matrix_set_color_all(RGB_WHITE);
     }
@@ -27,41 +20,25 @@ enum layer_names {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_4x6(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-        KC_LGUI, KC_LALT, KC_SPC,  MO(_FN1),MO(_FN2),KC_ENT
-    ),
+    [_BASE] = LAYOUT_charybdis_4x6(
+        // Левая сторона (29 клавиш)
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-    [_FN1] = LAYOUT_4x6(
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, MO(_BASE),_______, KC_TRNS
-    ),
-
-    [_FN2] = LAYOUT_4x6(
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, MO(_BASE),_______, KC_TRNS
+        // Правая сторона (27 клавиш)
+        KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_ENT,
+        KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RSFT,
+        KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,  KC_SPC,
+        KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+                          KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
 #ifdef VIA_ENABLE
-void via_custom_set_value(uint8_t page_id, uint8_t slot_id, uint8_t data[]) {
-    // =====================Custom config values=====================
-}
-
-void via_custom_get_value(uint8_t page_id, uint8_t slot_id, uint8_t *data) {
-    // =====================Custom config values=====================
-}
-
-void via_custom_save(void) {
-    // =====================Save custom config values=====================
-}
+void via_custom_set_value(uint8_t page_id, uint8_t slot_id, uint8_t data[]) { }
+void via_custom_get_value(uint8_t page_id, uint8_t slot_id, uint8_t *data) { }
+void via_custom_save(void) { }
 #endif
