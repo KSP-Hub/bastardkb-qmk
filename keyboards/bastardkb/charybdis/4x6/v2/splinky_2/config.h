92bf25b6a16df3e73cc46c15cf2250da20c9d500
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-04-02 17:12:05 +0300

    feat: enable USB detection and right side master

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define USB_VBUS_PIN GP25
-#define SPLIT_USB_DETECT
-#define MASTER_RIGHT
-#define RGB_MATRIX_LED_COUNT 58
-#define SOFT_SERIAL_PIN GP1
+#define USB_VBUS_PIN GP25        // Оживляет USB на контроллере Splinky
+#define SPLIT_USB_DETECT         // Помогает определять мастер-половину
+#define MASTER_RIGHT             // Кабель вставляется в правую половину (с трекболом)
+#define RGB_MATRIX_LED_COUNT 58  // Общее количество диодов
+#define SOFT_SERIAL_PIN GP1  // Параметры для «пробуждения» USB на RP2040
