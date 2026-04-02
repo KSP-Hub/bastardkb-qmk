73e4268f48d4013a5d69ed2ef12b2730f522e0c2
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-04-02 20:19:51 +0300

    fix: define RGB_MATRIX_DRIVER as ws2812

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -5,10 +5,10 @@ BOARD = PICO
 # Включаем основные функции
 SPLIT_KEYBOARD = yes
 MOUSEKEY_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
+POINTING_DEVICE_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 
-# Настройки трекбола:
-# Указываем конкретный драйвер для трекбола Charybdis
+# Драйверы (исправление ошибок 87 и 88)
 POINTING_DEVICE_DRIVER = pmw3360
+RGB_MATRIX_DRIVER = ws2812
 SERIAL_DRIVER = vendor
