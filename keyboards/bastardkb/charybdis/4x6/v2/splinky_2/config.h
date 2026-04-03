f39309f7be9b3c7ff9b86862b4dc9aace8401bbb
KSP <karamin.stanislav@gmail.com>
2026-04-03 15:48:50 +0300

    fix: define matrix settings and enable VIA support

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,25 +1,22 @@
 #pragma once
 
-// Пин для определения питания по USB
+// Питание и связь
 #define USB_VBUS_PIN GP25
-
-// Автоопределение подключения USB и сторона (Master - правая)
 #define SPLIT_USB_DETECT
 #define MASTER_RIGHT
 
-// МАТРИЦА (Pins для Charybdis 4x6 на Splinky)
+// МАТРИЦА (Настройки для Charybdis 4x6 на Splinky)
+#define MATRIX_ROWS 8  // 4 ряда на каждую сторону
+#define MATRIX_COLS 6  // 6 столбцов на каждую сторону
 #define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21 }
 #define MATRIX_COL_PINS { GP19, GP18, GP15, GP14, GP16, GP26 }
-#define DIODE_DIRECTION COL2ROW  // ЭТО ИСПРАВИТ ОШИБКУ #error
+#define DIODE_DIRECTION COL2ROW
 
-// Светодиоды
-#define RGB_MATRIX_LED_COUNT 58
-
-// Настройки SPI для трекбола (датчик PMW3360)
+// ТРЕКБОЛ (SPI настройки для PMW3360)
 #define PMW33XX_CS_PIN GP13
 #define SPI_SCK_PIN GP10
 #define SPI_MISO_PIN GP12
 #define SPI_MOSI_PIN GP11
 
-// Настройки связи (Split)
-// Пины для SERIAL и RGB закомментированы, так как QMK берет их из info.json
+// ПОДСВЕТКА
+#define RGB_MATRIX_LED_COUNT 58
