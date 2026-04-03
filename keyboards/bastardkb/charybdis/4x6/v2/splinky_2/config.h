624ca20c01466caef7176a7e79d570e3c8c84cdd
KSP <karamin.stanislav@gmail.com>
2026-04-03 16:15:48 +0300

    fix: disable RGB_MATRIX to bypass 4x6.c errors and get working

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -1,23 +1,21 @@
 #pragma once
 
-// Питание и связь
 #define USB_VBUS_PIN GP25
 #define SPLIT_USB_DETECT
 #define MASTER_RIGHT
 
-// МАТРИЦА (Юбилейная настройка для 100-й итерации)
-#define MATRIX_ROWS 8
+// Возвращаемся к 4 рядам, чтобы не злить 4x6.c
+#define MATRIX_ROWS 4
 #define MATRIX_COLS 6
-// Указываем пины для одной стороны, остальные QMK подтянет сам через SPLIT
-#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21, NO_PIN, NO_PIN, NO_PIN, NO_PIN }
+#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21 }
 #define MATRIX_COL_PINS { GP19, GP18, GP15, GP14, GP16, GP26 }
 #define DIODE_DIRECTION COL2ROW
 
-// ТРЕКБОЛ (SPI настройки)
 #define PMW33XX_CS_PIN GP13
 #define SPI_SCK_PIN GP10
 #define SPI_MISO_PIN GP12
 #define SPI_MOSI_PIN GP11
 
-// ПОДСВЕТКА
-#define RGB_MATRIX_LED_COUNT 58
+// Временно уменьшим число LED до физического на одну сторону,
+// чтобы массив в 4x6.c влез в память
+#define RGB_MATRIX_LED_COUNT 29
