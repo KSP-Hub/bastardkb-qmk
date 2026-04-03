74f2f5d06657fd71679d9e30c8b995c2d64ae22f
KSP <karamin.stanislav@gmail.com>
2026-04-03 16:47:28 +0300

    fix: align MATRIX_ROWS with keymap_introspection and disable RGB

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -4,10 +4,10 @@
 #define SPLIT_USB_DETECT
 #define MASTER_RIGHT
 
-// Возвращаемся к 4 рядам, чтобы не злить 4x6.c
-#define MATRIX_ROWS 4
+// Устанавливаем 8 рядов, чтобы формула в keymap_introspection.c сработала верно
+#define MATRIX_ROWS 8
 #define MATRIX_COLS 6
-#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21 }
+#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21, NO_PIN, NO_PIN, NO_PIN, NO_PIN }
 #define MATRIX_COL_PINS { GP19, GP18, GP15, GP14, GP16, GP26 }
 #define DIODE_DIRECTION COL2ROW
 
@@ -15,7 +15,3 @@
 #define SPI_SCK_PIN GP10
 #define SPI_MISO_PIN GP12
 #define SPI_MOSI_PIN GP11
-
-// Временно уменьшим число LED до физического на одну сторону,
-// чтобы массив в 4x6.c влез в память
-#define RGB_MATRIX_LED_COUNT 29
