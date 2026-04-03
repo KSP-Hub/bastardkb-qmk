7cf3e5c5ca4becfdb31af1a9f0f45b9f8ec74ec3
KSP <karamin.stanislav@gmail.com>
2026-04-03 14:43:41 +0300

    fix: use universal PMW33XX_CS_PIN on GP13 for sensor compatibility

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -13,11 +13,11 @@
 #define RGB_MATRIX_LED_COUNT 58
 
 // Настройки SPI для трекбола (датчик PMW3360)
-// ВАЖНО: Используем PMW33XX_CS_PIN на GP13, как требует драйвер QMK
-#define PMW33XX_CS_PIN GP13
-#define SPI_SCK_PIN GP10
-#define SPI_MISO_PIN GP12
-#define SPI_MOSI_PIN GP11
+// Используем универсальное имя PMW33XX, которое требует драйвер QMK
+#define PMW33XX_CS_PIN GP13 // Пин выбора чипа (Chip Select)
+#define SPI_SCK_PIN GP10 // Тактовый сигнал
+#define SPI_MISO_PIN GP12 // Вход данных
+#define SPI_MOSI_PIN GP11 // Выход данных
 
 // Пины для связи и RGB закомментированы (они в info.json)
 // #define SOFT_SERIAL_PIN GP1
