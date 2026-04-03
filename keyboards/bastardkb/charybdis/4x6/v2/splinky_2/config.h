d0d2d3efda8ae5791cebb495862016bd1a0dbcdb
KSP <karamin.stanislav@gmail.com>
2026-04-03 19:04:59 +0300

    fix(102): set MATRIX_ROWS to 5 to resolve layout boundary conflict

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -4,13 +4,15 @@
 #define SPLIT_USB_DETECT
 #define MASTER_RIGHT
 
-// Устанавливаем 8 рядов, чтобы формула в keymap_introspection.c сработала верно
-#define MATRIX_ROWS 8
+// МАТРИЦА: Ставим 5 рядов, чтобы обойти ошибку "must be less than 8"
+// и при этом покрыть все индексы макроса LAYOUT
+#define MATRIX_ROWS 5
 #define MATRIX_COLS 6
-#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21, NO_PIN, NO_PIN, NO_PIN, NO_PIN }
+#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21, NO_PIN }
 #define MATRIX_COL_PINS { GP19, GP18, GP15, GP14, GP16, GP26 }
 #define DIODE_DIRECTION COL2ROW
 
+// ТРЕКБОЛ
 #define PMW33XX_CS_PIN GP13
 #define SPI_SCK_PIN GP10
 #define SPI_MISO_PIN GP12
