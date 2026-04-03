fef93b5682c312d20daf2811fb7e31144191fe34
KSP <karamin.stanislav@gmail.com>
2026-04-03 19:15:08 +0300

    fix(103): use generic LAYOUT macro to bypass matrix row boundary errors

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -4,15 +4,13 @@
 #define SPLIT_USB_DETECT
 #define MASTER_RIGHT
 
-// МАТРИЦА: Ставим 5 рядов, чтобы обойти ошибку "must be less than 8"
-// и при этом покрыть все индексы макроса LAYOUT
-#define MATRIX_ROWS 5
+// Возвращаем честные 4 ряда для одной половины
+#define MATRIX_ROWS 4
 #define MATRIX_COLS 6
-#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21, NO_PIN }
+#define MATRIX_ROW_PINS { GP22, GP20, GP23, GP21 }
 #define MATRIX_COL_PINS { GP19, GP18, GP15, GP14, GP16, GP26 }
 #define DIODE_DIRECTION COL2ROW
 
-// ТРЕКБОЛ
 #define PMW33XX_CS_PIN GP13
 #define SPI_SCK_PIN GP10
 #define SPI_MISO_PIN GP12
