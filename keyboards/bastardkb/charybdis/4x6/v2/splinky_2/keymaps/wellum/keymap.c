f3cba5cd9bdfe83570303f83689ac7a851de348d
KSP <karamin.stanislav@gmail.com>
2026-04-03 15:00:32 +0300

    fix: reduce keymap arguments to 56 for 4x6 matrix

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/wellum/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// 1. Инициализация подсветки (белый цвет при старте)
 #ifdef RGB_MATRIX_ENABLE
 void keyboard_post_init_user(void) {
     if (is_keyboard_master()) {
@@ -10,33 +9,34 @@ void keyboard_post_init_user(void) {
 }
 #endif
 
-// 2. Определение названий слоев
 enum layer_names {
     _BASE,
     _FN1,
     _FN2
 };
 
-// 3. Основная раскладка (строго 30 позиций на сторону для 5x6 матрицы)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_charybdis_4x6(
-        // Левая половина (24 основных + 5 больших + 1 заглушка KC_NO)
+        // Левая половина (4 ряда по 6 клавиш = 24)
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
 
-        // Правая половина (24 основных + 3 больших + 3 заглушки KC_NO)
+        // Правая половина (4 ряда по 6 клавиш = 24)
         KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, KC_ENT,
         KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_RSFT,
         KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ESC,  KC_SPC,
         KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO
+
+        // Блок большого пальца (Thumb cluster) — суммарно 8 клавиш
+        // 5 клавиш слева:
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        // 3 клавиши справа:
+        KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
 
-// 4. Заглушки для VIA (чтобы не было ошибок при компиляции)
 #ifdef VIA_ENABLE
 void via_custom_set_value(uint8_t page_id, uint8_t slot_id, uint8_t data[]) { }
 void via_custom_get_value(uint8_t page_id, uint8_t slot_id, uint8_t *data) { }
