a4303d56fa44f24daca8d2e5d542fe4a1cb4152e
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-04-03 08:53:07 +0300

    fix: ensure MCU and BOARD are defined in rules.mk

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/rules.mk
@@ -7,8 +7,9 @@ SPLIT_KEYBOARD = yes
 MOUSEKEY_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
+VIA_ENABLE = yes
 
-# Драйверы (исправление ошибок 87 и 88)
+# Драйверы
 POINTING_DEVICE_DRIVER = pmw3360
 RGB_MATRIX_DRIVER = ws2812
 SERIAL_DRIVER = vendor
