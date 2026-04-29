ef98d2fabb5903e006a206499b705fb1cd091030
KSP <157570224+KSP-Hub@users.noreply.github.com>
2026-04-29 14:34:30 +0300

    feat: add vial support with key overrides for 4x6 layout

---
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/vial/config.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBE, 0xEF, 0x42, 0x69, 0x01, 0x02, 0x03, 0x04} // Случайный ID
+#define VIAL_UNLOCK_COMBO {0, 0} // Разблокировка настроек (нажать 0 и 1 клавишу вместе)
