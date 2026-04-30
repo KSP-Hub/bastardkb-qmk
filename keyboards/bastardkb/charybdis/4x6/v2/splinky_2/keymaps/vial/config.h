10d0fde2f7974be37d583698e31fb69e1c559ea0
KSP <karamin.stanislav@gmail.com>
2026-04-30 14:16:19 +0300

    fix(vial/config.h): wrap VIAL macros in #ifdef, add SPLIT_USB_DETECT, align with QMK standards

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/keymaps/vial/config.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "config_common.h"
+
 #define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
 #define ONESHOT_TAP_TOGGLE 2
@@ -9,11 +11,14 @@
 #define USE_SERIAL
 #define SOFT_SERIAL_PIN GP1
 
-// Ограничим слои до 4 для экономии RAM (хватит за глаза)
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
 
-#define VIAL_KEYBOARD_UID {0xBE, 0xEF, 0x42, 0x69, 0x01, 0x02, 0x03, 0x04}
-#define VIAL_UNLOCK_COMBO {0, 0}
+#ifdef VIA_ENABLE
+    #define VIAL_KEYBOARD_UID {0xBE, 0xEF, 0x42, 0x69, 0x01, 0x02, 0x03, 0x04}
+    #define VIAL_UNLOCK_COMBO {0, 0}
+#endif
 
-// Настройки трекбола
-#define POINTING_DEVICE_ROTATION_90 // Если трекбол инвертирован, удалите или замените
+#define POINTING_DEVICE_ROTATION_90
